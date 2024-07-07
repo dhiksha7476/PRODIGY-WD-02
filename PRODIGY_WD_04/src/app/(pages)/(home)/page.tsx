49bc5b2321d87ae6fc9bed0912078ec3033b333c
dhiksha7476 <dhikshabavi7476@gmail.com>
2024-07-07 11:11:56 +0530

    Create page.tsx

---
+++ PRODIGY_WD_04/src/app/(pages)/(home)/page.tsx
@@ -0,0 +1,14 @@
+<section className="about-text">
+        <p>
+          🙋‍♂️ Greetings! I'm Tilak Thapa(jrTilak), a passionate web developer
+          with more than 1 years of experience. I specialize web development in
+          React and Next.js. I'm also a huge fan of open-source. I'm currently
+          looking for opportunities to work as a Full Stack Developer.
+          🙋‍♂️ Greetings! I&apos;m Tilak Thapa(jrTilak), a passionate web
+          developer with more than 1 years of experience. I specialize web
+          development in React and Next.js. I&apos;m also a huge fan of
+          open-source. I&apos;m currently looking for opportunities to work as a
+           Full Stack Developer.
+        </p>
+
+        <p>
